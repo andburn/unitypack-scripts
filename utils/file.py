43221e12861656918d1cc693afd102b712b083d2
Andrew Burnett <andrew@andburn.info>
2016-06-15 20:04:15 +0100

    Force utf-8 when saving non binary files

--- utils/file.py
+++ utils/file.py
@@ -4,7 +4,8 @@ import os
 def write_to_file(path, contents, mode="w", info=False, warn=True):
 	if os.path.isfile(path) and warn:
 		print("WARNING: %s exists and will be overwritten" % (path))
-	with open(path, mode) as f:
+	encoding = None if "b" in mode else "utf-8"
+	with open(path, mode, encoding=encoding) as f:
 		written = f.write(contents)
 	if info:
 		print("Written %i bytes to %r" % (written, path))
@@ -14,9 +15,9 @@ def filename_no_ext(path):
 	return os.path.splitext(os.path.basename(path))[0];
 
 
-def get_file_path(basedir, filename):
-	path = os.path.join(basedir, filename)
+def make_dirs(path):
 	dirs = os.path.dirname(path)
 	if not os.path.exists(dirs):
 		os.makedirs(dirs)
-	return path
+		return True
+	return False
