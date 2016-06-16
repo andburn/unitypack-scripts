b254864569c65dd5beaa293605ed7a575fd909a3
Andrew Burnett <andrew@andburn.info>
2016-06-16 19:09:08 +0100

    Fix extraction scripts

--- extract.py
+++ extract.py
@@ -8,125 +8,101 @@ from unitypack.export import OBJMesh
 import json_mesh
 from argparse import ArgumentParser
 from PIL import ImageOps
-from fsb5 import FSB5
-import utils
+import utils.file as FileUtils
 
 
-def write_to_file(path, filename, contents, mode="w"):
-	file = utils.get_file_path(path, filename)
-	with open(file, mode) as f:
-		written = f.write(contents)
-
-
-def handle_asset(asset, handle_formats, dir, flip, obj):
+def handle_asset(asset, handle_formats, dir, flip, objMesh, quiet):
 	for id, obj in asset.objects.items():
-		if obj.type not in handle_formats:
+		try:
+			otype = obj.type
+		except Exception as e:
+			print("[Error] %s" % (e))
 			continue
 
-		save_path = os.path.join(dir, obj.type)
+		if otype not in handle_formats:
+			continue
+
 		d = obj.read()
+		save_path = os.path.join(dir, obj.type, d.name)
+		FileUtils.make_dirs(save_path)
 
-		if obj.type == "AudioClip":
-			if not d.data:
-				# eg. StreamedResource not available
-				continue
-			af = FSB5(d.data)
-			for i, sample in enumerate(af.samples):
-				if i > 0:
-					filename = "%s-%i.%s" % (d.name, i, af.get_sample_extension())
-				else:
-					filename = "%s.%s" % (d.name, af.get_sample_extension())
-				try:
-					sample = af.rebuild_sample(sample)
-				except ValueError as e:
-					print("WARNING: Could not extract %r (%s)" % (d, e))
-					continue
-				write_to_file(save_path, filename, sample, mode="wb")
-
-		elif obj.type == "MovieTexture":
-			filename = d.name + ".ogv"
-			write_to_file(save_path, filename, d.movie_data, mode="wb")
-
-		elif obj.type == "Shader":
-			write_to_file(save_path, d.name + ".cg", d.script)
-
-		elif obj.type == "Mesh":
+		if otype == "Mesh":
 			try:
 				mesh_data = None
 				ext = ".js"
-				if obj:
-					mesh_data = OBJMesh(d).export()
-					ext = ".obj"
+				if objMesh:
+				 	mesh_data = OBJMesh(d).export()
+				 	ext = ".obj"
 				else:
 					mesh_data = json_mesh.JSONMesh(d).export()
-				write_to_file(save_path, d.name + ext, mesh_data, mode="w")
-			except NotImplementedError as e:
+				FileUtils.write_to_file(save_path + ext, mesh_data, mode="w")
+			except (NotImplementedError, RuntimeError) as e:
 				print("WARNING: Could not extract %r (%s)" % (d, e))
 				mesh_data = pickle.dumps(d._obj)
-				write_to_file(save_path, d.name + ".Mesh.pickle", mesh_data, mode="wb")
+				FileUtils.write_to_file(save_path + ".Mesh.pickle", mesh_data, mode="wb")
 
-		elif obj.type == "TextAsset":
+		elif otype == "TextAsset":
 			if isinstance(d.script, bytes):
-				write_to_file(save_path, d.name + ".bin", d.script, mode="wb")
+				FileUtils.write_to_file(save_path + ".bin", d.script, mode="wb")
 			else:
-				write_to_file(save_path, d.name + ".txt", d.script)
+				FileUtils.write_to_file(save_path + ".txt", d.script)
 
-		elif obj.type == "Texture2D":
+		elif otype == "Texture2D":
 			filename = d.name + ".png"
 			image = d.image
 			if image is None:
 				print("WARNING: %s is an empty image" % (filename))
-				write_to_file(save_path, filename, "")
+				FileUtils.write_to_file(save_path + ".empty", "")
 			else:
-				print("Decoding %r" % (d))
+				if not quiet:
+					print("Decoding %r" % (d))
 				img = image
 				if flip:
 					img = ImageOps.flip(image)
-				path = utils.get_file_path(save_path, filename)
-				img.save(path)
+				img.save(save_path + ".png")
 
 
 def main():
 	p = ArgumentParser()
-	p.add_argument("input")
-	p.add_argument("output")
-	p.add_argument("--flip", action="store_true")
-	p.add_argument("--obj", action="store_true")
+	p.add_argument("files", nargs="+")
+	p.add_argument("--output", "-o", required=True)
 	p.add_argument("--all", action="store_true")
-	p.add_argument("--audio", action="store_true")
 	p.add_argument("--images", action="store_true")
 	p.add_argument("--models", action="store_true")
-	p.add_argument("--shaders", action="store_true")
 	p.add_argument("--text", action="store_true")
-	p.add_argument("--video", action="store_true")
+	p.add_argument("--quiet", action="store_true")
+	# flip images the "right" way up
+	p.add_argument("--flip", action="store_true")
+	# option for obj meshes (instead of js)
+	p.add_argument("--obj", action="store_true")
 	args = p.parse_args(sys.argv[1:])
 
 	format_args = {
-		"audio": "AudioClip",
 		"images": "Texture2D",
 		"models": "Mesh",
-		"shaders": "Shader",
 		"text": "TextAsset",
-		"video": "MovieTexture",
 	}
 	handle_formats = []
 	for a, classname in format_args.items():
 		if args.all or getattr(args, a):
 			handle_formats.append(classname)
 
-	files = glob.glob(args.input + "/*.unity3d")
+	files = args.files
+	if len(args.files) == 1:
+		if not args.files[0].endswith("unity3d"):
+			files = glob.glob(args.files + "/*.unity3d")
+
 	for file in files:
-		bundle_name = utils.filename_no_ext(file)
+		bundle_name = FileUtils.filename_no_ext(file)
 		print("Extracting %s..." % (bundle_name))
-		if bundle_name == "cardxml0":
-			continue
 		save_path = os.path.join(args.output, bundle_name)
 
 		with open(file, "rb") as f:
 			bundle = unitypack.load(f)
 
 		for asset in bundle.assets:
-			handle_asset(asset, handle_formats, save_path, args.flip, args.obj)
+			handle_asset(asset, handle_formats, save_path,
+						 args.flip, args.obj, args.quiet)
 
 
 if __name__ == "__main__":
--- extract_yaml.py
+++ extract_yaml.py
@@ -1,29 +1,46 @@
+#!/usr/bin/env python
 import os
 import sys
 import glob
 import unitypack
-import utils
-import obj_to_yaml
+import utils.file as FileUtils
+import utils.serializer as YamlSerializer
+
+
+FILE_EXT = ".unity3d"
+EXCLUDES = ["sounds0"]
 
 
 def main():
+	if len(sys.argv) != 3:
+		print("Usage: extract_yaml.py <dir_in> <dir_out>")
+		sys.exit(2)
+
 	dir_in = sys.argv[1]
 	dir_out = sys.argv[2]
+	files = glob.glob(dir_in + "/*" + FILE_EXT)
 
-	files = glob.glob(dir_in + '/*.unity3d')
-	yaml = obj_to_yaml.YamlWriter()
+	for f in files:
+		bundle_name = FileUtils.filename_no_ext(f)
+		if bundle_name in EXCLUDES:
+			print("Skipping %s%s" % (bundle_name, FILE_EXT))
+			continue
+		else:
+			out_path = os.path.join(dir_out, bundle_name)
+			print("Extracting %s%s" % (bundle_name, FILE_EXT))
 
-	for file in files:
-		save_path = os.path.join(dir_out, utils.filename_no_ext(file))
-
-		with open(file, "rb") as f:
-			bundle = unitypack.load(f)
+		with open(f, "rb") as fin:
+			bundle = unitypack.load(fin)
 
 		for asset in bundle.assets:
 			for id, obj in asset.objects.items():
-				d = obj.read()
-				f = utils.get_file_path(save_path, str(id) + ".yaml")
-				utils.write_to_file(f, yaml.write(d))
+				try:
+					d = obj.read()
+					file_out = os.path.join(out_path, str(id) + ".yaml")
+					FileUtils.make_dirs(file_out)
+					FileUtils.write_to_file(file_out, YamlSerializer.serialize(d), warn=False)
+				except Exception as e:
+					print("[Error] %s" % (e))
 
 
 if __name__ == "__main__":
