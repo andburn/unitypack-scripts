95a79a97b75e99df400fce6d3db7fced25cff0f0
Andrew Burnett <andrew@andburn.info>
2017-11-02 21:32:08 +0000

    parser: objects for instructiions and functions

--- glsl_parser.py
+++ glsl_parser.py
@@ -12,15 +12,11 @@ from pyparsing import (
 )
 
 
-LBRACE, RBRACE, LBRACK, RBRACK, LPAR, RPAR = map(Suppress, "{}[]()")
+LBRACE, RBRACE, LBRACK, RBRACK = map(Suppress, "{}[]")
 PLUS, DASH, SLASH, ASTERIX, PERCENT, EQ, DOT = map(Literal, "+-/*%=.")
 COLON, SEMI, COMMA, HASH, QUESTION = map(Suppress, ":;,#?")
-CARET, BAR, AMPERSAND, TILDE, BANG = map(Suppress, "^|&~!")
-LESS, GREAT = map(Literal, "<>")
-
-
-Function = namedtuple("Function", "name params")
-Unary = namedtuple("Unary", "name op")
+CARET, BAR, AMPERSAND, TILDE = map(Suppress, "^|&~")
+LESS, GREAT, LPAR, RPAR, BANG = map(Literal, "<>()!")
 
 
 class Identifier:
@@ -99,6 +95,68 @@ class Assignment:
 		return f"{self.type}({values})"
 
 
+class Function:
+	def __init__(self, name, params):
+		self.name = name
+		self.params = params
+
+	def __repr__(self):
+		return "Func(name={}, params={})".format(self.name, self.params)
+
+	def __str__(self):
+		return "{}({})".format(self.name, ", ".join(map(str, self.params)))
+
+
+class Instruction:
+	def __init__(self, ident, expr):
+		self.ident = ident
+		self.expression = expr
+
+	def __repr__(self):
+		return "Ins(ident={}, expr={})".format(self.ident, self.expression)
+
+	def __str__(self):
+		return "{} = {};".format(self.ident, " ".join(map(str, self.expression)))
+
+
+class Unary:
+	def __init__(self, op, param):
+		self.operation = op
+		self.param = param
+
+	def __repr__(self):
+		return "Unary(op={}, expr={})".format(self.operation, self.param)
+
+	def __str__(self):
+		return "{}{}".format(self.operation, self.param)
+
+
+class Binary:
+	def __init__(self, op, paraml, paramr):
+		self.operation = op
+		self.param_left = paraml
+		self.param_right = paramr
+
+	def __repr__(self):
+		return "Binary(op={} pl={} pr={}".format(self.operation, self.param_left, self.param_right)
+
+	def __str__(self):
+		return "{1} {0} {2}".format(self.operation, self.param_left, self.param_right)
+
+
+class IfBlock:
+	def __init__(self, comp, if_block, else_block):
+		self.comparison = comp
+		self.if_block = if_block
+		self.else_block = else_block
+
+	def __repr__(self):
+		return "If(comp={}, if={}, else={})".format(self.comparison, self.if_block, self.else_block)
+
+	def __str__(self):
+		return "if ({})".format(self.comparison)
+
+
 def new_ident(tokens):
 	ident = Identifier(None, None, None)
 	if "name" in tokens:
@@ -122,6 +180,25 @@ def new_assign(tokens):
 		d = tokens[0]
 		d.value = tokens[1]
 		return d
+
+
+def new_binary(tokens):
+	pass
+
+
+def new_if_block(tokens):
+	# check for the special case
+	if "discard_func" in tokens:
+		return InlineIf(tokens["discard_func"], "discard")
+	# determine whether its a if or an if/else
+	if_block = IfBlock(None, None, None)
+	if "if_block" in tokens:
+		if_block.comparison = tokens["if_comp"]
+		if_block.if_block = tokens["if_block"]
+		if "else_block" in tokens:
+			if_block.else_block = tokens["else_block"]
+
+	return if_block
 
 
 def parse(text):
@@ -188,7 +265,7 @@ def parse(text):
 	declaration.setParseAction(lambda t : new_declare(t))
 
 	# declaration with assignment
-	assignment_value = type_specifier + LPAR + delimitedList(const) + RPAR
+	assignment_value = type_specifier + Suppress(LPAR) + delimitedList(const) + Suppress(RPAR)
 	assignment_value.setParseAction(lambda t : Assignment(t[0], t[1:]))
 
 	assignment_expr = declaration + Suppress(EQ) + (assignment_value | const)
@@ -201,12 +278,12 @@ def parse(text):
 	function = Forward()
 
 	unary_expr = DASH + (function | ident_swizzle)
-	unary_expr.setParseAction(lambda t : Unary(t[1], t[0]))
+	unary_expr.setParseAction(lambda t : Unary(t[0], t[1]))
 
 	operand = function | unary_expr | ident_swizzle | const
 	function_param = binary_operation | operand
 
-	function << (builtin_functions + LPAR + delimitedList(function_param) + RPAR)
+	function << (builtin_functions + Suppress(LPAR) + delimitedList(function_param) + Suppress(RPAR))
 	binary_operation <<= (
 		(LPAR + binary_operation + RPAR + (operator | comparator) + operand)
 		| (LPAR + binary_operation + RPAR)
@@ -215,21 +292,31 @@ def parse(text):
 	)
 
 	function.setParseAction(lambda t : Function(t[0], t[1:]))
+	binary_operation.setParseAction(lambda t : new_binary(t))
 
 	comparison = operand + comparator + operand
+	# specific instance of ternary expressoin encountered
 	ternary_expr = LPAR + LPAR + comparison + RPAR + QUESTION + operand + COLON + operand + RPAR
 
 	expr = ternary_expr | binary_operation | unary_expr | function | ident_swizzle
 
-	instruction = ident_swizzle + EQ + expr + SEMI
+	instruction = ident_swizzle + Suppress(EQ) + expr + SEMI
+	instruction.setParseAction(lambda t : Instruction(t[0], t[1:]))
 
 	block = LBRACE + OneOrMore(instruction) + RBRACE
 
-	if_only = Literal("if") + LPAR + comparison + RPAR + block
-	if_else = if_only + Literal("else") + block
+	if_only = (
+		Literal("if") + LPAR + comparison.setResultsName("if_comp") + RPAR
+		+ block.setResultsName("if_block")
+	)
+	if_else = if_only + Literal("else") + block.setResultsName("else_block")
 	# one-off case for single line conditional discard
-	if_discard = Literal("if") + LPAR + function + RPAR + Literal("discard")
+	if_discard = (
+		Literal("if") + LPAR + function.setResultsName("discard_func")
+		+ RPAR + Literal("discard")
+	)
 	conditional = if_else | if_only | if_discard + SEMI
+	conditional.setParseAction(lambda t : new_if_block(t))
 
 	statements = instruction | conditional
 
@@ -238,7 +325,7 @@ def parse(text):
 
 	# main function
 	main_function = (
-		Suppress("void") + Suppress("main") + LPAR + RPAR + LBRACE
+		Suppress("void") + Suppress("main") + Suppress(LPAR) + Suppress(RPAR) + LBRACE
 		+ OneOrMore(statements).setResultsName("instructions")  + RBRACE
 	)
 
@@ -251,10 +338,26 @@ def parse(text):
 	return parser.parseString(text)
 
 
-def output(parsed):
+def output(parsed, tab="\t"):
 	print(f"#version {parsed.version}")
+
 	for decl in parsed.declarations:
-		print("{!r}".format(decl))
+		print("{!s}".format(decl))
+
+	print("\nvoid main()\n{")
+	for ins in parsed.instructions:
+		if isinstance(ins, IfBlock):
+			print("{}{} {{".format(tab, ins))
+			for inner_ins in ins.if_block:
+				print("{0}{0}{1}".format(tab, inner_ins))
+			if ins.else_block:
+				print("{}}} else {{".format(tab))
+				for inner_ins in ins.else_block:
+					print("{0}{0}{1}".format(tab, inner_ins))
+			print("{}}}".format(tab))
+		else:
+			print("{}{!s}".format(tab, ins))
+	print("}")
 
 
 def run_on_all(dir):
